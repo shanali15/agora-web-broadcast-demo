338035f1765e8fa7971120d8bded9c0676ab8231
shan <shanali15f@gmail.com>
2020-09-10 15:17:47 +0500

    Firebase/agora/multi audiance

---
+++ js/firebase.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDOLHtmuTUjqiqEX7m3FrUilVcp7jkHfyU",
+    authDomain: "safestrream.firebaseapp.com",
+    databaseURL: "https://safestrream.firebaseio.com",
+    projectId: "safestrream",
+    storageBucket: "safestrream.appspot.com",
+    messagingSenderId: "509329731331",
+    appId: "1:509329731331:web:64a4d16c1b5774dde98336"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
